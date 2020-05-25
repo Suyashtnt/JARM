13afbe03886f243e77a7d78d4c1dd5d5c97a1a97
KrazyMiner001 <KrazyMiner001@outlook.com>
2020-05-25 09:06:47 +0200

    Added recipes
    Added recipe for strengthened diamond block & flint and ruby

--- src/main/java/com/tntmaninc/jram/item/RubyDimensionItem.java
+++ src/main/java/com/tntmaninc/jram/item/RubyDimensionItem.java
@@ -8,17 +8,17 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.item.ItemUseContext;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.entity.player.PlayerEntity;
 
 import com.tntmaninc.jram.world.dimension.RubyDimensionDimension;
+import com.tntmaninc.jram.itemgroup.JARMCreativeTabItemGroup;
 
 public class RubyDimensionItem extends Item {
 	@ObjectHolder("jram:ruby_dimension")
 	public static final Item block = null;
 	public RubyDimensionItem() {
-		super(new Item.Properties().group(ItemGroup.TOOLS).maxDamage(64));
+		super(new Item.Properties().group(JARMCreativeTabItemGroup.tab).maxDamage(64));
 	}
 
 	@Override
