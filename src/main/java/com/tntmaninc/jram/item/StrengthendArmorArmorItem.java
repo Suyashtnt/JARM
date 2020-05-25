65bae10e12757d5e799e2c7367934fdc39cbfda0
KrazyMiner001 <KrazyMiner001@outlook.com>
2020-05-25 16:11:54 +0200

    modified ruby guardian
    made it less op and drop Ruby diamond sometimes
    also readded strong ruby armor

---
+++ src/main/java/com/tntmaninc/jram/item/StrengthendArmorArmorItem.java
@@ -0,0 +1,92 @@
+
+package com.tntmaninc.jram.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.Entity;
+
+import com.tntmaninc.jram.itemgroup.JARMCreativeTabItemGroup;
+import com.tntmaninc.jram.JramModElements;
+
+@JramModElements.ModElement.Tag
+public class StrengthendArmorArmorItem extends JramModElements.ModElement {
+	@ObjectHolder("jram:strengthend_armor_armorhelmet")
+	public static final Item helmet = null;
+	@ObjectHolder("jram:strengthend_armor_armorbody")
+	public static final Item body = null;
+	@ObjectHolder("jram:strengthend_armor_armorlegs")
+	public static final Item legs = null;
+	@ObjectHolder("jram:strengthend_armor_armorboots")
+	public static final Item boots = null;
+	public StrengthendArmorArmorItem(JramModElements instance) {
+		super(instance, 50);
+	}
+
+	@Override
+	public void initElements() {
+		IArmorMaterial armormaterial = new IArmorMaterial() {
+			public int getDurability(EquipmentSlotType slot) {
+				return new int[]{13, 15, 16, 11}[slot.getIndex()] * 45;
+			}
+
+			public int getDamageReductionAmount(EquipmentSlotType slot) {
+				return new int[]{6, 18, 15, 6}[slot.getIndex()];
+			}
+
+			public int getEnchantability() {
+				return 27;
+			}
+
+			public net.minecraft.util.SoundEvent getSoundEvent() {
+				return (net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation(""));
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+
+			@OnlyIn(Dist.CLIENT)
+			public String getName() {
+				return "strengthend_armor_armor";
+			}
+
+			public float getToughness() {
+				return 0f;
+			}
+		};
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.HEAD, new Item.Properties().group(JARMCreativeTabItemGroup.tab)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "jram:textures/models/armor/strengthendarmor_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("strengthend_armor_armorhelmet"));
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.CHEST, new Item.Properties().group(JARMCreativeTabItemGroup.tab)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "jram:textures/models/armor/strengthendarmor_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("strengthend_armor_armorbody"));
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.LEGS, new Item.Properties().group(JARMCreativeTabItemGroup.tab)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "jram:textures/models/armor/strengthendarmor_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("strengthend_armor_armorlegs"));
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.FEET, new Item.Properties().group(JARMCreativeTabItemGroup.tab)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "jram:textures/models/armor/strengthendarmor_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("strengthend_armor_armorboots"));
+	}
+}
